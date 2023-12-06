1428afb7c9a11cc2623bf90676e8ab7ed1c8a3e6
8o8Live <143171006+8o8Live@users.noreply.github.com>
2023-12-06 21:05:45 +0100

    Updates

--- videoscript.js
+++ videoscript.js
@@ -1,11 +1,11 @@
-
- const video = document.getElementById('myVideo');
+document.addEventListener("DOMContentLoaded", function () {
+  const video = document.getElementById('myVideo');
   let hasPlayedOnce = false; // Variable to track if the video has played at least once
 
   // Set the playback rate to 0.5 (half of the normal speed)
   video.playbackRate = 0.5;
 
-  // Function to unmute the video when it comes into view
+  // Function to handle the intersection changes
   function handleVideo(entries, observer) {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
@@ -14,10 +14,10 @@
           video.play(); // Start playing if it hasn't played yet
           hasPlayedOnce = true;
         }
-        observer.unobserve(entry.target); // Stop observing once it's visible
       } else {
         if (hasPlayedOnce) {
           video.pause(); // Pause the video if it goes out of view after playing once
+          observer.disconnect(); // Disconnect the observer to stop further observations
         }
       }
     });
@@ -28,3 +28,14 @@
 
   // Start observing the video element
   observer.observe(video);
+
+  // Add event listener for scroll
+  window.addEventListener('scroll', handleScroll);
+
+  // Function to handle scroll event
+  function handleScroll() {
+    video.pause(); // Pause the video when the user starts scrolling
+    observer.disconnect(); // Disconnect the observer to stop further observations
+    window.removeEventListener('scroll', handleScroll); // Remove the scroll event listener
+  }
+});
